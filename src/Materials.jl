50eae52a892000c28ac14ae34f78aa1454d3b278
Jukka Aho <ahojukka5@gmail.com>
2018-09-13 19:54:47 +0300

    Temporary hack to make package work with Julia 1.0
    * Do not include old code to make package work. (Old code can be
      included after it is fixed.)
    * Update REQUIRE and .travis.yml

--- src/Materials.jl
+++ src/Materials.jl
@@ -3,16 +3,8 @@
 
 module Materials
 
-include("types.jl")
-export Material, IsotropicHooke, Plastic, VonMises, Model, create_material
+using FEMBase
 
-include("plasticity.jl")
-export yield_function, d_yield_function, equivalent_stress
-
-include("solvers.jl")
-export find_root, radial_return, array_to_tensor, tensor_to_array
-
-include("response.jl")
-export calc_response!
+# include("olli.jl")
 
 end
