db4d115bdbe13101bad4b8ef33d6c6da4f599a18
Tero Frondelius <tero.frondelius@gmail.com>
2019-08-28 22:07:55 +0300

    MFront_Norton function to initiate MFront type

--- src/mfront.jl
+++ src/mfront.jl
@@ -1,22 +1,28 @@
 # This file is a part of JuliaFEM.
 # License is MIT: see https://github.com/JuliaFEM/Materials.jl/blob/master/LICENSE
 
-mutable struct MFront <: AbstractMaterial
+mbv = MFrontInterface.behaviour
 
-    mbv = MFrontInterface.behaviour
+mutable struct MFront <: AbstractMaterial
+    behaviour::BehaviourAllocated
+    behaviour_data::BehaviourDataAllocated
+    offset::Integer
+    equivalent_plastic_strain::AbstractFloat
+
+end
+
+function MFront_Norton()
     behaviour = load("data/libBehaviour.so","Norton", mbv.Tridimensional)
     behaviour_data = BehaviourData(behaviour)
-    set_time_increment!(behaviour_data, dtime)
 
     offset = get_variable_offset(get_internal_state_variables(behaviour),
-                                "EquivalentViscoplasticStrain",
-                                get_hypothesis(behaviour))
+                                 "EquivalentViscoplasticStrain",
+                                 get_hypothesis(behaviour))
 
     set_external_state_variable!(get_final_state(behaviour_data), "Temperature", 293.15)
-
     update(behaviour_data)
 
-    return MFront(behaviour, behaviour_data, offset, )
+    return MFront(behaviour, behaviour_data, offset, 0.0)
 end
 
 function integrate_material!(material::Material{MFront})
@@ -24,6 +30,7 @@ function integrate_material!(material::Material{MFront})
     behaviour = mat.behaviour
     behaviour_data = mat.behaviour_data
     offset = mat.offset
+    eps = mat.equivalent_plastic_strain
 
     stress = material.stress
     strain = material.strain
@@ -33,10 +40,21 @@ function integrate_material!(material::Material{MFront})
     time = material.time
     dtime = material.dtime
 
+    set_time_increment!(behaviour_data, dtime)
+    update(behaviour_data)
+
     # Maybe use LibrariesManager::getInternalStateVariablesNames
+
+    gradients = get_gradients(get_final_state(behaviour_data))
+    gradients[1] += dstrain[1]
+    gradients[2] += dstrain[2]
+    gradients[3] += dstrain[3]
+    gradients[4] += dstrain[4]
+    gradients[5] += dstrain[5]
+    gradients[6] += dstrain[6]
 
     integrate(behaviour_data, behaviour)
     update(behaviour_data)
-    get_gradients(get_final_state(behaviour_data))[1] += de
-    push!(p,get_internal_state_variables(get_final_state(behaviour_data))[offset])
+
+    eps = get_internal_state_variables(get_final_state(behaviour_data))[offset]
 end
