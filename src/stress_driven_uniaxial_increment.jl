7c0e78df5adcd6b73e6c9c6b7efbd8bb3581633e
Veli-Jussi Haanpää <veli-jussi.haanpaa@oulu.fi>
2020-04-03 10:39:14 +0300

    Working model and tests

--- src/stress_driven_uniaxial_increment.jl
+++ src/stress_driven_uniaxial_increment.jl
@@ -1,96 +1,23 @@
 # This file is a part of JuliaFEM.
 # License is MIT: see https://github.com/JuliaFEM/Materials.jl/blob/master/LICENSE
 
-using NLsolve
-
-function stress_driven_uniaxial_increment!(material, dstrain11, dtime; 
-        dstrain=[dstrain11, -0.3*dstrain11, -0.3*dstrain11, 0.0, 0.0, 0.0],
-        max_iter=50, norm_acc=1e-9)
+ function stress_driven_uniaxial_increment!(material, dstress11,
+        dt; dstrain=[dstress11/200e3, -0.3*dstress11/200e3, -0.3*dstress11/200e3, 0.0, 0.0, 0.0], max_iter=50, norm_acc=1e-9)
     converged = false
     stress0 = tovoigt(material.variables.stress)
-    println("************************************")
-    println("stress0:    ", typeof(stress0), ", size; ", size(stress0), "\n", dstrain)
-    println("------------------------------------")
     for i=1:max_iter
-        #dstrainguess = [dstrain11, -0.3*dstrain11, -0.3*dstrain11, 0.0, 0.0, 0.0]
-        #f!(creepres, tosolve) = creepresidual!(creepres, tosolve, material, dtime, stress0)
-        #dstrain = nlsolve( f!, dstrainguess ).zero # , autodiff = :forward 
-        
-        material.ddrivers.time = dtime
+        material.ddrivers.time = dt
         material.ddrivers.strain = fromvoigt(SymmetricTensor{2,3,Float64}, dstrain; offdiagscale=2.0)
         integrate_material!(material)
         stress = tovoigt(material.variables_new.stress)
         dstress = stress - stress0
-        
-        dstress11 = dstress[1]
-        stresslimit = stress[1]
-
-        if stresslimit >= material.parameters.yield_stress
-            dstress[1:end] .= 0.0
-            creepstress = [material.parameters.yield_stress, 0.0, 0.0, 0.0, 0.0, 0.0]
-            newstress = fromvoigt(SymmetricTensor{2,3,Float64}, creepstress; offdiagscale=2.0)
-
-            E       = material.parameters.youngs_modulus
-            nu      = material.parameters.poissons_ratio
-
-            dstrain = [(1/E)*dstress[1], -(nu/E)*dstress[1], -(nu/E)*dstress[1], 0.0, 0.0, 0.0] .* 0.0
-            material.drivers.strain  = fromvoigt(SymmetricTensor{2,3,Float64}, dstrain; offdiagscale=2.0)
-            material.ddrivers.strain = fromvoigt(SymmetricTensor{2,3,Float64}, dstrain; offdiagscale=2.0)
-
-
-            println("------------------------------------")
-            println("dstrain(1): ", typeof(dstrain), ", size; ", size(dstrain), "\n", dstrain)
-            println("------------------------------------")
-            
-            material.variables_new = IdealPlasticVariableState(
-                stress          = newstress,
-                plastic_strain  = material.variables_new.plastic_strain,
-                cumeq           = material.variables_new.cumeq,
-                jacobian        = material.variables_new.jacobian)
-
-        else
-            dstress[2:end] .= 0.0
-        end
-
+        r = dstress 
+        r[1] -= dstress11
         D = tovoigt(material.variables_new.jacobian)
-        dstr = -D[2:end,2:end]\dstress[2:end]
-        dstrain[2:end] .+= dstr
-        
-        stress = tovoigt(material.variables_new.stress)
-
-        println("stress:     ", typeof(stress), ", size; ", size(stress), "\n", stress)
-        println("dstress:    ", typeof(dstress), ", size; ", size(dstress), "\n", dstress)
-        println("dstrain:    ", typeof(dstrain), ", size; ", size(dstrain), "\n", dstrain)
-        println("------------------------------------")
-
+        dstr = -D\r
+        dstrain .+= dstr
         norm(dstr) < norm_acc && (converged = true; break)
     end
-
     converged || error("No convergence in strain increment")
     return nothing
-end
-
-function creepresidual!(creepres, tosolve, material, dtime, stress0)
-
-    #guess = [dstrain11, -0.3*dstrain11, -0.3*dstrain11, 0.0, 0.0, 0.0]
-    #f!() = residual!(material, dtime, tosolve)
-    #dstrain = nlsolve( f!, guess, autodiff = :forward ).zero
-
-    dstrain::Array{Float64,1}   = tosolve
-    tempmaterial                = material
-
-    tempmaterial.ddrivers.time   = dtime
-    tempmaterial.ddrivers.strain = fromvoigt(SymmetricTensor{2,3,Float64}, dstrain; offdiagscale=2.0)
-    
-    integrate_material!(tempmaterial)
-    
-    #stress0     = tovoigt(tempmaterial.variables.stress)
-    stress      = tovoigt(tempmaterial.variables_new.stress)
-    dstress     = stress - stress0
-    creepstress = tempmaterial.parameters.yield_stress
-    creepvector = [creepstress, 0.0, 0.0, 0.0, 0.0, 0.0]
-    
-    creepres    = dstress - creepvector
-    
-    return creepres, tosolve
 end
--- test/test_stress_driven_uniaxial_increment.jl
+++ test/test_stress_driven_uniaxial_increment.jl
@@ -1,60 +1,51 @@
 # This file is a part of JuliaFEM.
 # License is MIT: see https://github.com/JuliaFEM/Materials.jl/blob/master/LICENSE
-using Test, Tensors, Materials, Plots
+
+using Test, Tensors, Materials
+parameters = ChabocheParameterState(E = 200.0e3,
+                                    nu = 0.3,
+                                    R0 = 100.0,
+                                    Kn = 100.0,
+                                    nn = 10.0,
+                                    C1 = 10000.0,
+                                    D1 = 100.0,
+                                    C2 = 50000.0,
+                                    D2 = 1000.0,
+                                    Q = 0.0,
+                                    b = 0.1)
+material = Chaboche(parameters = parameters)
+
 dtime = 0.25
-# material = Material(IdealPlastic, tuple())
-# material.properties.youngs_modulus = 200.0e3
-# material.properties.poissons_ratio = 0.3
-# material.properties.yield_stress = 100.0
-parameters = IdealPlasticParameterState(youngs_modulus = 200.0e3,
-                                        poissons_ratio = 0.3,
-                                        yield_stress = 100.0)
-material = IdealPlastic(parameters=parameters)
 times = [material.drivers.time]
 stresses = [copy(tovoigt(material.variables.stress))]
+strains = [copy(tovoigt(material.drivers.strain; offdiagscale=2.0))]
+cumeqs = [copy(material.variables.cumeq)]
+
 stresses_expected = [[       50.0, 0.0, 0.0, 0.0, 0.0, 0.0],
                      [      100.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                     [      150.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                     [      150.0, 0.0, 0.0, 0.0, 0.0, 0.0],
                      [      100.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-                     [      100.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-                     [      100.0, 0.0, 0.0, 0.0, 0.0, 0.0]]
-dstrain11 = 1e-3*dtime # == 1/200e3 * 50.0 
+                     [     -100.0, 0.0, 0.0, 0.0, 0.0, 0.0]]
+dstress = 50.0 
 
-E       = material.parameters.youngs_modulus
-nu      = material.parameters.poissons_ratio
-
-strains_expected = [[(1/E)*stresses_expected[1][1], -(nu/E)*stresses_expected[1][1], -(nu/E)*stresses_expected[1][1], 0.0, 0.0, 0.0],
-                    [(1/E)*stresses_expected[2][1], -(nu/E)*stresses_expected[2][1], -(nu/E)*stresses_expected[2][1], 0.0, 0.0, 0.0],
-                    [(1/E)*stresses_expected[3][1], -(nu/E)*stresses_expected[3][1], -(nu/E)*stresses_expected[3][1], 0.0, 0.0, 0.0].*2,
-                    [(1/E)*stresses_expected[4][1], -(nu/E)*stresses_expected[4][1], -(nu/E)*stresses_expected[4][1], 0.0, 0.0, 0.0].*3,
-                    [(1/E)*stresses_expected[5][1], -(nu/E)*stresses_expected[5][1], -(nu/E)*stresses_expected[5][1], 0.0, 0.0, 0.0].*4]
-dtimes      = [dtime, dtime, dtime, dtime, 1.0]
-dstrains11  = [dstrain11, dstrain11, dstrain11, dstrain11, dstrain11]
+dtimes      = [dtime, dtime, dtime, 1e3, dtime, 1e3]
+dstresses11  = [dstress, dstress, dstress, 0.0, -dstress, -4*dstress]
 for i in 1:length(dtimes)
-    dstrain11 = dstrains11[i]
+    dstress11 = dstresses11[i]
     dtime = dtimes[i]
-    stress_driven_uniaxial_increment!(material, dstrain11, dtime)
-    # stress_driven_uniaxial_increment!(material, dstrain11, dtime; dstrain = copy(tovoigt(material.ddrivers.strain))*dstrain11/material.ddrivers.strain[1,1]*dtime/material.ddrivers.time)
-    # material.time += material.dtime
-    # material.strain .+= material.dstrain
-    # material.stress .+= material.dstress
+    stress_driven_uniaxial_increment!(material, dstress11, dtime)
     update_material!(material)
-    # push!(times, material.drivers.time)
-    # push!(stresses, copy(tovoigt(material.variables.stress)))
-    #@info(tovoigt(material.variables.stress), stresses_expected[i])
-    #@test isapprox(tovoigt(material.variables.stress), stresses_expected[i])
-    #@test isapprox(tovoigt(material.drivers.strain; offdiagscale=2.0), strains_expected[i])
+    push!(times, material.drivers.time)
+    push!(stresses, copy(tovoigt(material.variables.stress)))
+    push!(strains, copy(tovoigt(material.drivers.strain; offdiagscale=2.0)))
+    push!(cumeqs, copy(material.variables.cumeq))
+    @test isapprox(tovoigt(material.variables.stress), stresses_expected[i]; atol=1e-4)
 end
 
-xx = [0,strains_expected[1][1],strains_expected[2][1],strains_expected[3][1],strains_expected[4][1],strains_expected[5][1]]
-yy = [0,stresses_expected[1][1],stresses_expected[2][1],stresses_expected[3][1],stresses_expected[4][1],stresses_expected[5][1]]
+dstrain_creep = strains[5] - strains[4]
+@test isapprox(dstrain_creep[2], -dstrain_creep[1]*0.5; atol=1e-4)
+@test isapprox(dstrain_creep[3], -dstrain_creep[1]*0.5; atol=1e-4)
 
-pyplot()
-blot=plot(xx, yy,
-            title = "Stress-driven Uniaxial Increment, Ideal Plastic (11)",
-            xlabel = "Stress",
-            ylabel = "Strain",
-            xlims = (0,0.002),
-            ylims = (0,125),
-            yticks = 0:25:125)
-display(blot)
-png(blot,"SDUI.png")
+dcumeq = cumeqs[end] - cumeqs[end-1]
+@test dcumeq > 0
