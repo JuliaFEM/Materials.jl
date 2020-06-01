8352fe7862e8d0691781a363e38d94a21f00a20c
Juha Jeronen <juha.m.jeronen@gmail.com>
2020-06-01 14:38:35 +0300

    Cleanup vol. 2

--- src/chaboche.jl
+++ src/chaboche.jl
@@ -1,15 +1,12 @@
 # This file is a part of JuliaFEM.
 # License is MIT: see https://github.com/JuliaFEM/Materials.jl/blob/master/LICENSE
 
-# TODO: parametrize the short aliases on the element type?
-typealias SymmT2 SymmetricTensor{2,3}
-typealias SymmT4 SymmetricTensor{4,3}
-typealias SymmT2Float64 SymmetricTensor{2,3,Float64}
-typealias SymmT4Float64 SymmetricTensor{4,3,Float64}
+const Symm2{T} = SymmetricTensor{2,3,T}
+const Symm4{T} = SymmetricTensor{4,3,T}
 
 @with_kw mutable struct ChabocheDriverState <: AbstractMaterialState
     time :: Float64 = zero(Float64)
-    strain :: SymmT2 = zero(SymmT2Float64)
+    strain :: Symm2 = zero(Symm2{Float64})
 end
 
 @with_kw struct ChabocheParameterState <: AbstractMaterialState
@@ -27,13 +24,13 @@ end
 end
 
 @with_kw struct ChabocheVariableState <: AbstractMaterialState
-    stress :: SymmT2 = zero(SymmT2Float64)
-    X1 :: SymmT2 = zero(SymmT2Float64)
-    X2 :: SymmT2 = zero(SymmT2Float64)
-    plastic_strain :: SymmT2 = zero(SymmT2Float64)
+    stress :: Symm2 = zero(Symm2{Float64})
+    X1 :: Symm2 = zero(Symm2{Float64})
+    X2 :: Symm2 = zero(Symm2{Float64})
+    plastic_strain :: Symm2 = zero(Symm2{Float64})
     cumeq :: Float64 = zero(Float64)
     R :: Float64 = zero(Float64)
-    jacobian :: SymmT4 = zero(SymmT4Float64)
+    jacobian :: Symm4 = zero(Symm4{Float64})
 end
 
 @with_kw mutable struct Chaboche <: AbstractMaterial
@@ -46,45 +43,87 @@ end
 end
 
 # Convert the elastic constants (E, ν) to the Lamé constants (μ, λ). Isotropic material.
-@inline function lame(E, ν)
+@inline function lame(E::Real, ν::Real)
     μ = E/(2.0*(1.0+nu))
     λ = E*nu/((1.0+nu)*(1.0-2.0*nu))
     return μ, λ
 end
 
 # Adaptors for NLsolve. Marshal the problem state into a Vector and back.
-@inline function state_to_vector(σ::SymmT2Float64,
-                                 R::Float64,
-                                 X1::SymmT2Float64,
-                                 X2::SymmT2Float64)
+@inline function state_to_vector(σ::T, R::S, X1::T, X2::T) where T <: Symm2{S} where S <: Real
     return [tovoigt(σ), R, tovoigt(X1), tovoigt(X2)]
 end
-@inline function state_from_vector(x::Vector{Float64})
-    σ = fromvoigt(SymmT2Float64, @view x[1:6])
+@inline function state_from_vector(x::AbstractVector{Real})
+    σ = fromvoigt(Symm2{S}, @view x[1:6])
     R = x[7]
-    X1 = fromvoigt(SymmT2Float64, @view x[8:13])
-    X2 = fromvoigt(SymmT2Float64, @view x[14:19])
+    X1 = fromvoigt(Symm2{S}, @view x[8:13])
+    X2 = fromvoigt(Symm2{S}, @view x[14:19])
     return σ, R, X1, X2
 end
 
+# f!(F, x) -> f(x)
+#
 # Take away the bang; i.e. produce an equivalent single-argument non-mutating
 # function f (that allocates and returns the result), when given a two-argument
 # mutating function f! (which writes its result into the first argument).
 #
-# The argument and result types of F! must be the same.
+# When the types, sizes and shapes of the output F are the same as those for the
+# input x, it is enough to supply just f!.
 #
-# TODO: To generalize, we could make a @generated function that reflects on the
-# parameter types of f!, and creates `out` using the type of f!'s first argument
-# (instead of using `x` as we do now). Of course we're still relying on the type having
-# a sensible default constructor (to perform the allocation when similar() is called).
-function debang(f!)
-    function f(x)
-        out = similar(x)
-        f!(out, x)
-        return out
+# When the output type, size and/or shape is different from the input, then an
+# example instance of the correct type with the correct size and shape for the
+# output must be supplied, as the ex argument. It is only used to allocate
+# uninitialized memory with the right type, size and shape whenever f is called.
+#
+# (While the type of F is known at compile time, the size and shape are
+# typically runtime properties, not encoded into the type. For example, arrays
+# have the number of dimensions encoded into the type, but the length of each
+# dimension is defined at run time, when an instance is created.)
+#
+function debang(f!, ex=nothing)
+    if ex === nothing
+        function f(x)
+            out = similar(x)
+            f!(out, x)
+            return out
+        end
+    else
+        function f(x)
+            out = similar(ex)
+            f!(out, x)
+            return out
+        end
     end
     return f
 end
+
+# function debang(f!)
+#     # Adapted from
+#     # https://white.ucc.asn.au/2018/10/03/Dispatch,-Traits-and-Metaprogramming-Over-Reflection.html
+#     arg1type(f::Function) = map(arg1type, methods(f)) # check each method of function
+#     arg1type(m::Method) = arg1type(m.sig)
+#     arg1type(x::Any) = throw(x, ArgumentError("unsupported argument type"))
+#     arg1type(::Type{Tuple{F, T1, VarArg{Any}}}) where {F, T1} = T1
+#
+#     arg2type(f::Function) = map(arg2type, methods(f)) # check each method of function
+#     arg2type(m::Method) = arg2type(m.sig)
+#     arg2type(x::Any) = throw(x, ArgumentError("unsupported argument type"))
+#     arg2type(::Type{Tuple{F, T1, T2, VarArg{Any}}}) where {F, T1, T2} = T2
+#
+#     mm = collect(methods(f!))
+#     if length(mm) > 1
+#         throw(ArgumentError(f!, "debang is only implemented for functions with a single method"))
+#     end
+#     themethod = mm[1]
+#
+#     T = arg1type(themethod)
+#     function f(x)
+#         out = similar(T)  # can't work, required information not present in T alone.
+#         f!(out, x)
+#         return out
+#     end
+#     return f
+# end
 
 function integrate_material!(material::Chaboche)
     p = material.parameters
@@ -97,16 +136,16 @@ function integrate_material!(material::Chaboche)
     @unpack strain, time = d
     dstrain = dd.strain
     dtime = dd.time
-    @unpack stress, X1, X2, plastic_strain, cumeq, R, jacobian = v
+    @unpack stress, X1, X2, plastic_strain, cumeq, R = v
 
     # elastic part
     jacobian = isotropic_elasticity_tensor(lambda, mu)  # dσ/dε, i.e. ∂σij/∂εkl
-    stress += dcontract(jacobian, dstrain)  # add the elastic stress increment, get the intermediate stress
+    stress += dcontract(jacobian, dstrain)  # add the elastic stress increment, get the elastic trial stress
 
     # resulting deviatoric plastic stress (accounting for backstresses Xm)
     seff_dev = dev(stress - X1 - X2)
     # von Mises yield function; f := J(seff_dev) - Y
-    f = sqrt(1.5)*norm(seff_dev) - (R0 + R)  # using intermediate problem state, after elastic update
+    f = sqrt(1.5)*norm(seff_dev) - (R0 + R)  # using elastic trial problem state
     if f > 0.0
         g! = create_nonlinear_system_of_equations(material)
         x0 = state_to_vector(stress, R, X1, X2)
@@ -115,15 +154,16 @@ function integrate_material!(material::Chaboche)
         x = res.zero
         stress, R, X1, X2 = state_from_vector(x)
 
+        # using the new problem state
         seff_dev = dev(stress - X1 - X2)
-        f = sqrt(1.5)*norm(seff_dev) - (R0 + R)  # using new problem state
+        f = sqrt(1.5)*norm(seff_dev) - (R0 + R)
 
         dotp = ((f >= 0.0 ? f : 0.0)/Kn)^nn  # plasticity multiplier, see equations (3) and (4) in Chaboche 2013
         dp = dotp*dtime  # |dε_p|, using backward Euler (dotp is ∂ε_p/∂t at the end of the timestep)
         n = sqrt(1.5)*seff_dev/norm(seff_dev)  # Chaboche: a (tensorial) unit direction, s.t. 2/3 * (n : n) = 1
 
         plastic_strain += dp*n
-        cumeq += dp   # TODO: Some kind of 1D cumulative plastic strain? (Note dp ≥ 0.) What is this used for?
+        cumeq += dp   # cumulative equivalent plastic strain (note dp ≥ 0)
 
         # Compute the new Jacobian, accounting for the plastic contribution. Because
         #   x ≡ [σ R X1 X2]   (vector of length 19, with tensors encoded in Voigt format)
@@ -138,7 +178,7 @@ function integrate_material!(material::Chaboche)
         # We are only interested in dσ/dε, so the rest of drde can be left as zeros.
         # TODO: where does the minus sign come from?
         drde[1:6, 1:6] = -tovoigt(jacobian)  # (negative of the) elastic Jacobian. Follows from the defn. of g!.
-        jacobian = fromvoigt(SymmT4, (drdx\drde)[1:6, 1:6])
+        jacobian = fromvoigt(Symm4, (drdx\drde)[1:6, 1:6])
     end
     variables_new = ChabocheVariableState(stress = stress,
                                           X1 = X1,
@@ -158,17 +198,21 @@ function create_nonlinear_system_of_equations(material::Chaboche)
     @unpack E, nu, R0, Kn, nn, C1, D1, C2, D2, Q, b = p
     mu, lambda = lame(E, nu)
 
-    # Store the old problem state (i.e. the problem state at the time when this
-    # equation system instance was created).
+    # Old problem state (i.e. the problem state at the time when this equation
+    # system instance was created).
+    #
+    # Note this does not include the elastic trial; this is the state at the
+    # end of the previous timestep.
     @unpack strain, time = d
     dstrain = dd.strain
     dtime = dd.time
     @unpack stress, X1, X2, plastic_strain, cumeq, R = v
 
+    jacobian = isotropic_elasticity_tensor(lambda, mu)
+
     # Compute the residual. F is output, x is filled by NLsolve.
     # The solution is x = x* such that g(x*) = 0.
-    function g!(F, x::Vector{T}) where {T}
-        jacobian = isotropic_elasticity_tensor(lambda, mu)  # TODO: why not compute once, store in closure?
+    function g!(F::V, x::V) where V <: AbstractVector{Real}
         stress_, R_, X1_, X2_ = state_from_vector(x)  # tentative new values from nlsolve
 
         seff_dev = dev(stress_ - X1_ - X2_)
@@ -181,9 +225,9 @@ function create_nonlinear_system_of_equations(material::Chaboche)
         # The equations are written in a delta form:
         #
         # Δσ = (∂σ/∂ε)_e : dε_e = (∂σ/∂ε)_e : (dε - dε_p)   (components 1:6)
-        # ΔR = b (Q - R_new) |dε|                           (component 7)
-        # ΔX1 = (2/3) C1 |dε| (n - (3/2) (D1/C1) X1_new)    (components 8:13)
-        # ΔX2 = (2/3) C2 |dε| (n - (3/2) (D2/C2) X2_new)    (components 14:19)
+        # ΔR = b (Q - R_new) |dε_p|                         (component 7)
+        # ΔX1 = (2/3) C1 |dε_p| (n - (3/2) (D1/C1) X1_new)  (components 8:13)
+        # ΔX2 = (2/3) C2 |dε_p| (n - (3/2) (D2/C2) X2_new)  (components 14:19)
         #
         # where
         #
@@ -195,18 +239,9 @@ function create_nonlinear_system_of_equations(material::Chaboche)
         dstrain_elastic = dstrain - dstrain_plastic
         tovoigt!(view(F, 1:6), stress - stress_ + dcontract(jacobian, dstrain_elastic))
         F[7] = R - R_ + b*(Q - R_)*dp
-        if isapprox(C1, 0.0)
-            # TODO: In order for the below general case to reduce to this, both C1 **and D1** must be zero.
-            tovoigt!(view(F, 8:13), X1 - X1_)
-        else
-            tovoigt!(view(F, 8:13), X1 - X1_ + 2.0/3.0*C1*dp*(n - 1.5*D1/C1*X1_))
-        end
-        if isapprox(C2, 0.0)
-            # TODO: In order for the below general case to reduce to this, both C2 **and D2** must be zero.
-            tovoigt!(view(F, 14:19), X2 - X2_)
-        else
-            tovoigt!(view(F, 14:19), X2 - X2_ + 2.0/3.0*C2*dp*(n - 1.5*D2/C2*X2_))
-        end
+        # dp is a scalar, so it commutes in multiplication. This allows us to avoid the division by C1.
+        tovoigt!(view(F,  8:13), X1 + (-1.0 + dp*(2.0/3.0*C1*n - D1*X1_)))
+        tovoigt!(view(F, 14:19), X2 + (-1.0 + dp*(2.0/3.0*C2*n - D2*X2_)))
     end
     return g!
 end
