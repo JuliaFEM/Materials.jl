d81ec8e780398d07c5bfb720a1ff856b0ae2b951
Juha Jeronen <juha.m.jeronen@gmail.com>
2020-06-10 13:28:13 +0300

    Clean up testset naming

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,20 +7,20 @@ using Materials, Test
     @testset "test utilities" begin
         include("test_utilities.jl")
     end
-    @testset "test ideal plastic material model" begin
+    @testset "test ideal plastic uniaxial stress" begin
         include("test_idealplastic.jl")
     end
     @testset "test ideal plastic pure shear" begin
         include("test_idealplastic_shear.jl")
     end
-    @testset "test uniaxial increment" begin
-        include("test_uniaxial_increment.jl")
+    @testset "test chaboche uniaxial stress" begin
+        include("test_chaboche.jl")
     end
     @testset "test chaboche pure shear" begin
         include("test_chaboche_shear.jl")
     end
-    @testset "test chaboche uniaxial stress" begin
-        include("test_chaboche.jl")
+    @testset "test uniaxial increment" begin
+        include("test_uniaxial_increment.jl")
     end
     @testset "test biaxial increment" begin
         include("test_biaxial_increment.jl")
