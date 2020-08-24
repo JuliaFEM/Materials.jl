9a3dbd13e06609aa1d11bcacae91b7ec0e21208c
Juha Jeronen <juha.m.jeronen@gmail.com>
2020-08-24 16:03:10 +0300

    add textbook citation for perfect plastic

--- src/perfectplastic.jl
+++ src/perfectplastic.jl
@@ -52,6 +52,14 @@ PerfectPlasticVariableState = GenericPerfectPlasticVariableState{Float64}
 
 Perfect plastic material: no hardening. The elastic region remains centered on the
 origin, and retains its original size.
+
+This is a standard basic plasticity model; see a textbook, such as:
+
+    J. C. Simo, T. J. R. Hughes. Computational Inelasticity. Interdisciplinary
+    Applied Mathematics volume 7. Springer. 1998. http://dx.doi.org/10.1007/b98904
+
+    The notation in the book somewhat differs from ours; see:
+    https://github.com/JuliaFEM/Materials.jl/pull/66#issuecomment-674786955
 """
 function integrate_material!(material::GenericPerfectPlastic{T}) where T <: Real
     p = material.parameters
